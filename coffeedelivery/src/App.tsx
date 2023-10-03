a37905a4a28cd9f43d11b7e0a58c2d33c055be85
Henrique Samensari Borges <45943336+rickborges93@users.noreply.github.com>
2023-10-03 17:27:29 -0300

    Routes and route layout

--- coffeedelivery/src/App.tsx
+++ coffeedelivery/src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
